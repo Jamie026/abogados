8fa9b6bd90ede6173da2da946a504d1882a95f1d
wanly13 <wanlyobregon@gmail.com>
2025-04-10 21:29:25 -0500

    documento

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ require("dotenv").config();
 const PORT = process.env.port || 3000;
 
 const cliente = require("./routes/cliente");
+const abogados = require("./routes/abogados");
+const casos = require("./routes/casos");
 
 app.use(cors());
 app.use(express.json());
 app.use("/clientes", cliente);
-app.use("/abogados", cliente);
+app.use("/abogados", abogados);
+app.use("/casos", casos);
 
 app.listen(PORT, () => {
-    console.log("Server Listening in", PORT);  
+    console.log("Server Listening in", PORT);
 });
